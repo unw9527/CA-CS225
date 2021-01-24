6b92dac24786fab66866ebd0ffef81905253baf2
sc-1000 <77871908+sc-1000@users.noreply.github.com>
2021-01-24 11:24:59 +0800

    2323

--- Assignment-1/alist.cpp
+++ Assignment-1/alist.cpp
@@ -14,13 +14,11 @@ using std::cin;
 
 /* Function: delete_last_k
  * Input: int k
- * Output: none
+ * Output: none if k>=1, "Invalid number of elements deleted!\n" if k<1
  * Return value: none
  * Description: delete the last k elements from AList as required in Assignment 1.1
  */
 template<class T> void AList<T>::delete_last_k(int k){
-    if ((numitems == maxsize / 4) && (maxsize > minsize))
-        deallocate(); 
     if (k < 1){
         cout << "Invalid number of elements deleted!\n";
         return;
@@ -28,11 +26,12 @@ template<class T> void AList<T>::delete_last_k(int k){
     if (k <= numitems)
     {
         numitems -= k; // decrease the number of items if k <= numitems
-        return;
     }
     else{
         numitems = 0; // set the list length to 0 if k > numitems
     }
+    while ((numitems <= maxsize / 4) && (maxsize > minsize))
+        deallocate(); 
 }
 
 template<class T> T &AList<T>::operator[](int index)
@@ -213,27 +212,66 @@ template<class T> bool AList<T>::sublist(AList<T> &list)
     }
     return result;
 }
-template<class T>void AList<T>::selectionSort(void){	//Sort in ascending order
-	for(int i=0;i<numitems;i++){
-		int minn=reprarray[i],min_id=i;
-		for(int j=i+1;j<numitems;j++){
-			if(minn>reprarray[j]){
-				minn=reprarray[j];
-				min_id=j;
-			}
-		}
-		reprarray[min_id]=reprarray[i];
-		reprarray[i]=minn;
-	}
+
+/* Function: src(e,f,g,l)
+ * Input: T_t e,T_t (*f)(T_t),T_t (*g)(T_t,T_t),AList<T> &list
+ * Output: none
+ * Return value type: T_t
+ * Description: when l is empty, return e;when l has one element, return f(x);when l has more elements, return g; just as required in Assignment 2
+ */
+/*========================================================*/
+//Problem 2.4
+template<typename T_t>T_t f_fun(T_t x){
+	return (T_t)x+5;	//here is an example of f
+}
+template<typename T_t>T_t g_fun(T_t x,T_t y){
+	return x+y;		//here is an example of g
+}
+template<class T>
+template<typename T_t>T_t AList<T>::src(T_t e,T_t (*f)(T_t),T_t (*g)(T_t,T_t),AList<T> &list){
+	if(list.numitems==0)return e;	//when l is empty, return e
+	if(list.numitems==1)return f(list.reprarray[0]);	//when l has one element, return f(x)
+	AList<T> l1,l2;		//initialize l1,l2
+	for(int i=0;i<list.numitems/2;i++)
+		l1.append(list.reprarray[i]);	//use for loop to let l1 be a sublist of l
+	for(int i=list.numitems/2;i<list.numitems;i++)
+		l2.append(list.reprarray[i]);	//use for loop to let l1 be a sublist of l, and l1+l2=l
+	return g(src(e,f,g,l1),src(e,f,g,l2));	//then just return g
+}
+/*========================================================*/
+/* Function: selectionSort
+ * Input: none
+ * Output: none
+ * Return value: none
+ * Description: use selection sort to sort AList, here it's in ascending order
+ */
+template<class T>void AList<T>::selectionSort(void){ 	//Sort in ascending order
+	for(int i=0;i<numitems;i++){ 	//use for loop to search through all the elements
+    	int minn=reprarray[i],min_id=i;  	//initialize minn and min_id
+  		for(int j=i+1;j<numitems;j++){	 //use for loop
+   			if(minn>reprarray[j]){  	//if there is one element smaller than minn
+    			minn=reprarray[j];	 //let minn be that element
+    			min_id=j; 	//let min_id be the index
+   			}
+  		}
+  		reprarray[min_id]=reprarray[i];  
+  		reprarray[i]=minn; 	//replace the smallest element with the i-th element
+ 	}
 } 
-template<class T>void AList<T>::bubbleSort(void){	//Sort in ascending order
-	for(int i=0;i<numitems-1;i++){
-		for(int j=0;j<numitems-i-1;j++){
-			if(reprarray[j]>=reprarray[j+1]){
-				int t=reprarray[j];
-				reprarray[j]=reprarray[j+1];
-				reprarray[j+1]=t;
-			}
+/* Function: bubbleSort
+ * Input: none
+ * Output: none
+ * Return value: none
+ * Description: use bubble sort to sort AList, here it's in ascending order
+ */
+template<class T>void AList<T>::bubbleSort(void){ 	//Sort in ascending order
+	for(int i=0;i<numitems-1;i++){ 	//use for loop to search through all the elements
+    	for(int j=0;j<numitems-i-1;j++){  	//use for loop to search the rest elements
+       		if(reprarray[j]>=reprarray[j+1]){  	//if j-th element is bigger than the j+1-th
+        	int t=reprarray[j];
+        	reprarray[j]=reprarray[j+1];
+        	reprarray[j+1]=t;  	//swap them 
+            }
 		}
-	}
+ 	}
 } 
