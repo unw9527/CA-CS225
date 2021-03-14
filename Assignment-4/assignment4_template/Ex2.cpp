eed5ae5f0190ad112e46c8dafbbcbc9d74c45ffd
Kunle Li <kunle.19@intl.zju.edu.cn>
2021-03-14 23:23:51 +0800

    hw4 done

--- Assignment-4/assignment4_template/Ex2.cpp
+++ Assignment-4/assignment4_template/Ex2.cpp
@@ -1,3 +1,4 @@
+  
 #include<iostream>	
 #include<math.h>
 #include <cstdlib>
@@ -13,6 +14,9 @@ template<class T> int NumSet<T>::Check_Relation(vector<pair<int,int> > R){
         0: The binary relation R is not symmetric
         1: The binary relation R is symmetric
     */
+    if (R.size()==0){
+    	return 0;
+	}			//for the case when the array is empty
    int max = R[0].first;
    for (int i = 0; i < R.size(); i++){
        if (max < R[i].first){
@@ -23,30 +27,27 @@ template<class T> int NumSet<T>::Check_Relation(vector<pair<int,int> > R){
        }
    } // find the max value to determine m for making unique hash value for each input
    int m = max + 1;
-   int table1[m*m+m]={0};
-   int count = 0; // 0 if two pairs share the same hash value, 1 otherwise
+   int same=0,count=0,eq=0;
+   vector< vector<int> > table1(m*17+m*13);
+  	// 0 if two pairs share the same hash value, 1 otherwise
    for (int i = 0; i < R.size(); i++){
        if (R[i].first == R[i].second){
-           continue; // if the first element in a pair is the same as the second, skip it
+       		eq++;
+            continue; // if the first element in a pair is the same as the second, skip it
        }
-        if (R[i].first > R[i].second){
-            int temp = R[i].first;
-            R[i].first = R[i].second;
-            R[i].second = temp;
-            // make sure the smaller element is put first in order to yield same hash value
-            // for symmetric pairs
-        }
-        int hashVal = R[i].second * m + R[i].first; // hash function
-        if (0 == table1[hashVal]){
-            table1[hashVal] = 1;
-            count++;
-        }
-        else if (1 == table1[hashVal]){
-            table1[hashVal] = 0;
-            count--;
-        }   
+        int hashVal1 = R[i].first * 17 + R[i].second*13;
+        int hashVal2 = R[i].second * 17 + R[i].first*13; // hash function
+        bool flag=0;
+        for(int j = 0;j<table1[hashVal1].size();j++){
+        	if(R[i].first==table1[hashVal1][j]){flag=1;break;}
+		}		//use for loop to finish hash function
+		if(flag){same++;continue;}
+		table1[hashVal1].push_back(R[i].first);
+        for(int j = 0;j<table1[hashVal2].size();j++){
+        	if(R[i].second==table1[hashVal2][j]){count++;break;}
+		}		//use for loop to finish hash function
     }
-    return (!count);
+    return R.size()==(eq+same+count*2);		//return 0 or 1 to finish it
 };
 int main(){
     int Set_A[10]={1,2,3,4,5,6,7,8,9,10};
@@ -84,6 +85,29 @@ int main(){
     cout << "For Test 3\n(Only contains pairs with the first element equals to the second):"<<endl;
     cout << "The correct answer should be Yes"<<endl;
     cout << "Your answer is:" << ((S.Check_Relation(Relation_R3)== 1)?"Yes":"No")<< endl;
+    
+    int Set_B[9]={19,25,73,17,55,92,78,84,92};
+    NumSet<int> F(Set_B,9);
+    vector<pair<int,int> > Relation_R4;
+    for (int i=0; i<9; i++){
+        Relation_R4.push_back(make_pair(Set_B[i],Set_B[i]));
+        for (int j=0; j<9; j++){
+            if (i == j+8)
+                Relation_R4.push_back(make_pair(Set_B[i],Set_B[j]));
+        }
+    }
+    cout << "For Test 4\n(Contain repeat element):"<<endl;
+    cout << "The correct answer should be No"<<endl;
+    cout << "Your answer is:" << ((F.Check_Relation(Relation_R4) == 1)?"Yes":"No")<< endl;
+    
+
+    int Set_D[0]={};
+    NumSet<int> D(Set_D,0);
+    vector<pair<int,int> > Relation_R5;
+
+    cout << "For Test 5\n(Contain no element):"<<endl;
+    cout << "The correct answer should be No"<<endl;
+    cout << "Your answer is:" << ((D.Check_Relation(Relation_R5) == 1)?"Yes":"No")<< endl;
     
     return 0;
 }
