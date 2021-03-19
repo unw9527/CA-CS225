ef1622fab87106e0afde12607b2d55ee40ccf8f8
Kunle Li <kunle.19@intl.zju.edu.cn>
2021-03-19 21:29:18 +0800

    test network

---
+++ Assignment-5/Assignment 5 template/ex2/multiset.h
@@ -0,0 +1,48 @@
+
+#ifndef _MULTISET_H_
+#define _MULTISET_H_
+
+
+/* Put your decleration here */
+template<class T> MultiSet{
+    public:
+        MultiSet();
+        /* You should add other functions to make the structure work  */
+
+
+        /* The following is required for this assignment */
+        /* It is not complete, you should design the APIs by yourself */
+        void insertion();
+        void deletion();
+        void retrieval();
+
+        void setunion();
+        void intersection();
+        void difference();
+
+    private:
+
+}
+
+
+template<class T> MultiSet_Chaining{
+    public:
+        MultiSet_Chaining();
+        /* You should add other functions to make the structure work  */
+
+
+
+        /* The following is required for this assignment */
+        /* It is not complete, you should design the APIs by yourself */
+        void insertion();
+        void deletion();
+        void retrieval();
+
+        void setunion();
+        void intersection();
+        void difference();
+
+    private:
+
+}
+#endif
