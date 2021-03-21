ca33bf0a0b4bb25db8c24668e2dc0d4643e53bf8
sc-1000 <77871908+sc-1000@users.noreply.github.com>
2021-03-21 18:51:37 +0800

    as5 test

--- Assignment-5/Assignment 5 template/ex2/ex2_test.cpp
+++ Assignment-5/Assignment 5 template/ex2/ex2_test.cpp
@@ -66,10 +66,11 @@ int main(){
     mul2.deletion(12345);
     mul2.print_pairs();
 
-    int input3[] = {1, 1, 1, 2, 2, 3, 4000};
-    int input4[] = {0, 2, 3, 3, 3, 3, 5, 5, 42};
+    double input3[] = {1.5, 1.5, 1.5, 2.1, 2.5, 3.5, 4000};
+    double input4[] = {0, 2.5, 3.6, 3.6, 3.6, 3.6, 5.6, 5.5, 42};
 
-    MultiSet<int> test3, test4;
+    MultiSet<double> test3;
+    MultiSet<double> test4;
     for(auto i: input3){
         test3.insertion(i);
     }
@@ -82,25 +83,63 @@ int main(){
     test4.print_pairs();
 
     cout << "Retrieval from test 3:" << endl;
-    test3.retrieval(1);
-    test3.retrieval(2);
+    test3.retrieval(1.5);
+    test3.retrieval(2.1);
     test3.retrieval(4000);
     test3.retrieval(178329);
 
     cout << "Setunion of test 3 and test 4: " << endl;
-    MultiSet<int> mul3;
+    MultiSet<double> mul3;
     mul3.setunion(&test3, &test4);
     mul3.print_pairs();
 
     cout << "Intersection of test 3 and test 4: " << endl;
-    MultiSet<int> mul4;
+    MultiSet<double> mul4;
     mul4.intersection(&test3, &test4);
     mul4.print_pairs();
 
     cout << "Difference of test 3 and test 4: " << endl;
-    MultiSet<int> mul5;
+    MultiSet<double> mul5;
     mul5.difference(&test3, &test4);
     mul5.print_pairs();
+
+
+    float input7[] = {1.4, 1.4, 1.5, 2.1, 2.5, 3.6, 4000};
+    float input8[] = {0, 2.5, 3.2, 3.3, 3.6, 3.6, 5.6, 5.5, 42};
+
+    MultiSet<float> test7;
+    MultiSet<float> test8;
+    for(auto i: input7){
+        test7.insertion(i);
+    }
+    for(auto i: input8){
+        test8.insertion(i);
+    }
+    cout << "Test 7: " << endl;
+    test7.print_pairs();
+    cout << "Test 8: " << endl;
+    test8.print_pairs();
+
+    cout << "Retrieval from test 7:" << endl;
+    test7.retrieval(1.4);
+    test7.retrieval(2.1);
+    test7.retrieval(4000);
+    test7.retrieval(178329);
+
+    cout << "Setunion of test 7 and test 8: " << endl;
+    MultiSet<float> mul7;
+    mul7.setunion(&test7, &test8);
+    mul7.print_pairs();
+
+    cout << "Intersection of test 7 and test 8: " << endl;
+    MultiSet<float> mul8;
+    mul8.intersection(&test7, &test8);
+    mul8.print_pairs();
+
+    cout << "Difference of test 7 and test 8: " << endl;
+    MultiSet<float> mul9;
+    mul9.difference(&test7, &test8);
+    mul9.print_pairs();
 
 
     /* Test for part(ii) */
