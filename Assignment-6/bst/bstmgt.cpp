754eb2e8ffafa7477915782048502ad5f8b9b97b
Kunle Li <kunle.19@intl.zju.edu.cn>
2021-03-27 10:52:50 +0800

    ex2 done

--- Assignment-6/bst/bstmgt.cpp
+++ Assignment-6/bst/bstmgt.cpp
@@ -15,146 +15,149 @@ using std::endl;
 
 int main()
 {
-    // int type, op;
-    // BST<int> mytree1 = BST<int>();
-    // BST<double> mytree2 = BST<double>();
-    // bool boolresult;
-    // do
-    // {
-    //     cout << "Choose your element type:\n";
-    //     cout << "0: int  1: double\n";
-    //     cin >> type ;
-    //     if ((type != 0) && (type != 1))
-    //         cout << "You must choose 0 or 1\n\n";
-    // }
-    // while ((type != 0) && (type != 1));
-    // do
-    // {
-    //     do
-    //     {
-    //         cout << "Choose an operation:\n";
-    //         cout << "O: quit\n";
-    //         cout << "1: insert\n";
-    //         cout << "2: delete\n";
-    //         cout << "3: find\n";
-    //         cout << "4: display\n";
-    //         cin >> op ;
-    //     }
-    //     while ((op < 0) && (op > 4));
-    //     switch(op)
-    //     {
-    //         case 0: return(0);
-    //         case 1:
-    //         {
-    //             cout << "Enter a value:\n";
-    //             if (type == 0)
-    //             {
-    //                 int intitem;
-    //                 cin >> intitem;
-    //                 mytree1.insert(intitem);
-    //             }
-    //             else
-    //             {
-    //                 double doubleitem;
-    //                 cin >> doubleitem;
-    //                 mytree2.insert(doubleitem);
-    //             }
-    //             break;
-    //         }
-    //         case 2:
-    //         {
-    //             cout << "Enter a value:\n";
-    //             if (type == 0)
-    //             {
-    //                 int intitem;
-    //                 cin >> intitem;
-    //                 mytree1.remove(intitem);
-    //             }
-    //             else
-    //             {
-    //                 double doubleitem;
-    //                 cin >> doubleitem;
-    //                 mytree2.remove(doubleitem);
-    //             }
-    //             break;
-    //         }
-    //         case 3:
-    //         {
-    //             cout << "Enter a value:\n";
-    //             if (type == 0)
-    //             {
-    //                 int intitem;
-    //                 cin >> intitem;
-    //                 boolresult = mytree1.find(intitem);
-    //             }
-    //             else
-    //             {
-    //                 double doubleitem;
-    //                 cin >> doubleitem;
-    //                 boolresult = mytree2.find(doubleitem);
-    //             }
-    //             if (boolresult == true)
-    //                 cout << "The value is in the BST.\n";
-    //             else
-    //                 cout << "The value is not in the BST.\n";
-    //             break;
-    //         }
-    //         case 4:
-    //         {
-    //             if (type == 0)
-    //                 mytree1.display();
-    //             else
-    //                 mytree2.display();
-    //         }
-    //     }
-    // }
-    // while (op != 0);
-    int k = 2;
+    int type, op;
     BST<int> mytree1 = BST<int>();
-    for (int i = 1; i < 7; i++){
-        mytree1.insert(i);
+    BST<double> mytree2 = BST<double>();
+    bool boolresult;
+    do
+    {
+        cout << "Choose your element type:\n";
+        cout << "0: int  1: double\n";
+        cin >> type ;
+        if ((type != 0) && (type != 1))
+            cout << "You must choose 0 or 1\n\n";
     }
-    cout << "mytree1: " << endl;
-    mytree1.display();
-    cout << "Delete 3: " << endl;
-    mytree1.remove(3);
-    mytree1.display();
-    cout << "Delete 5: " << endl;
-    mytree1.remove(5);
-    mytree1.display();
-    cout << "Delete 1: " << endl;
-    mytree1.remove(1);
-    mytree1.display();
-    cout << "Delete 6: " << endl;
-    mytree1.remove(6);
-    mytree1.display();
+    while ((type != 0) && (type != 1));
+    do
+    {
+        do
+        {
+            cout << "Choose an operation:\n";
+            cout << "O: quit\n";
+            cout << "1: insert\n";
+            cout << "2: delete\n";
+            cout << "3: find\n";
+            cout << "4: display\n";
+            cin >> op ;
+        }
+        while ((op < 0) && (op > 4));
+        switch(op)
+        {
+            case 0: return(0);
+            case 1:
+            {
+                cout << "Enter a value:\n";
+                if (type == 0)
+                {
+                    int intitem;
+                    cin >> intitem;
+                    mytree1.insert(intitem);
+                }
+                else
+                {
+                    double doubleitem;
+                    cin >> doubleitem;
+                    mytree2.insert(doubleitem);
+                }
+                break;
+            }
+            case 2:
+            {
+                cout << "Enter a value:\n";
+                if (type == 0)
+                {
+                    int intitem;
+                    cin >> intitem;
+                    mytree1.remove(intitem);
+                }
+                else
+                {
+                    double doubleitem;
+                    cin >> doubleitem;
+                    mytree2.remove(doubleitem);
+                }
+                break;
+            }
+            case 3:
+            {
+                cout << "Enter a value:\n";
+                if (type == 0)
+                {
+                    int intitem;
+                    cin >> intitem;
+                    boolresult = mytree1.find(intitem);
+                }
+                else
+                {
+                    double doubleitem;
+                    cin >> doubleitem;
+                    boolresult = mytree2.find(doubleitem);
+                }
+                if (boolresult == true)
+                    cout << "The value is in the BST.\n";
+                else
+                    cout << "The value is not in the BST.\n";
+                break;
+            }
+            case 4:
+            {
+                if (type == 0)
+                    mytree1.display();
+                else
+                    mytree2.display();
+            }
+        }
+    }
+    while (op != 0);
 
-    BST<int> mytree2 = BST<int>();
-    for (int i = 7; i > 1; i--){
-        mytree2.insert(i);
-    }
-    cout << "mytree2: " << endl;
-    mytree2.display();
-    cout << "Delete 3: " << endl;
-    mytree2.remove(3);
-    mytree2.display();
-    cout << "Delete 5: " << endl;
-    mytree2.remove(5);
-    mytree2.display();
-    cout << "Delete 2: " << endl;
-    mytree2.remove(2);
-    mytree2.display();
-    cout << "Insert 100: " << endl;
-    mytree2.insert(100);
-    mytree2.display();
-    cout << "Delete 2: " << endl;
-    mytree2.remove(2);
-    mytree2.display();
-    cout << "Delete 100: " << endl;
-    mytree2.remove(100);
-    mytree2.display();
-    cout << "Delete 4: " << endl;
-    mytree2.remove(4);
-    mytree2.display();
+    // below are the tests I used to debug.
+
+    // int k = 2;
+    // BST<int> mytree1 = BST<int>();
+    // for (int i = 1; i < 7; i++){
+    //     mytree1.insert(i);
+    // }
+    // cout << "mytree1: " << endl;
+    // mytree1.display();
+    // cout << "Delete 3: " << endl;
+    // mytree1.remove(3);
+    // mytree1.display();
+    // cout << "Delete 5: " << endl;
+    // mytree1.remove(5);
+    // mytree1.display();
+    // cout << "Delete 1: " << endl;
+    // mytree1.remove(1);
+    // mytree1.display();
+    // cout << "Delete 6: " << endl;
+    // mytree1.remove(6);
+    // mytree1.display();
+
+    // BST<int> mytree2 = BST<int>();
+    // for (int i = 7; i > 1; i--){
+    //     mytree2.insert(i);
+    // }
+    // cout << "mytree2: " << endl;
+    // mytree2.display();
+    // cout << "Delete 3: " << endl;
+    // mytree2.remove(3);
+    // mytree2.display();
+    // cout << "Delete 5: " << endl;
+    // mytree2.remove(5);
+    // mytree2.display();
+    // cout << "Delete 2: " << endl;
+    // mytree2.remove(2);
+    // mytree2.display();
+    // cout << "Insert 100: " << endl;
+    // mytree2.insert(100);
+    // mytree2.display();
+    // cout << "Delete 2: " << endl;
+    // mytree2.remove(2);
+    // mytree2.display();
+    // cout << "Delete 100: " << endl;
+    // mytree2.remove(100);
+    // mytree2.display();
+    // cout << "Delete 4: " << endl;
+    // mytree2.remove(4);
+    // mytree2.display();
     return(0);
 }
